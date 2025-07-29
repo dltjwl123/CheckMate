9b734d62bdba5a2bc9a506d7a7961e60360aca80
seonlim <dltjwl1@naver.com>
2025-07-29 12:28:13 +0900

    axios 인스턴스의 baseURL을 AWS에서 seonlim API로 변경

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://mathreview-alp-1105650430.ap-northeast-2.elb.amazonaws.com/",
+  baseURL: "https://api.seonlim.site",
   withCredentials: true,
   headers: { "Content-Type": "application/json" },
 })
