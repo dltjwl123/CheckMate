e56f67c38d155beb84790f1760b4d6d208bf57e8
seonlim <dltjwl1@naver.com>
2025-08-01 21:49:08 +0900

    이미지 설정에 원격 패턴 추가

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      new URL("https://math-review-test.s3.ap-northeast-2.amazonaws.com/**"),
+    ],
+  },
 };
 
 export default nextConfig;
